4a550465e269f44e82640845fd22d70a35f4d06c
Pavelliss <kovrizhkoPavel@gmail.com>
2020-12-10 17:36:41 +0300

    шаблон сортировки, disabled не активным полям

--- src/view/trip-sort.js
+++ src/view/trip-sort.js
@@ -1,20 +1,20 @@
 import AbstractView from "./adstract";
 import {makeСapitalizedLetter} from "../utils/util";
 
-const sortElementTemplate = (type, isChecked = false) => {
+const sortElementTemplate = (type, isDesabled = false, isChecked = false) => {
   return `<div class="trip-sort__item  trip-sort__item--${type}">
-    <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${isChecked ? `checked` : false}>
+    <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${isChecked ? `checked` : false} ${isDesabled ? `disabled` : ``}>
     <label class="trip-sort__btn" for="sort-${type}">${makeСapitalizedLetter(type)}</label>
   </div>`;
 };
 
 const tripSortTemplate = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    ${sortElementTemplate(`day`, true)}
-    ${sortElementTemplate(`event`)}
+    ${sortElementTemplate(`day`, false, true)}
+    ${sortElementTemplate(`event`, true)}
     ${sortElementTemplate(`time`)}
     ${sortElementTemplate(`price`)}
-    ${sortElementTemplate(`offer`)}
+    ${sortElementTemplate(`offer`, true)}
   </form>`;
 };
 
